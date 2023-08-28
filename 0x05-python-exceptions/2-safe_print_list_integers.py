7150c7caa9a7d1466502cab45af9201400205998
douniabendib <bendib.dounia@gmail.com>
2023-08-28 23:47:04 +0000

    print list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+def safe_print_list_integers(my_list=[], x=0):
+    print_integer = 0
+    try:
+        for item in my_list:
+            try:
+                formatt_item = "{:d}".format(item)
+                print(formatt_item, end=' ')
+                print_integer += 1
+                if print_integer >= x:
+                    break
+            except ValueError:
+                pass
+    except IndexError:
+        pass 
+
+    print() 
+    return print_integer
