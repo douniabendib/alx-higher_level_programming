b633507fffe98cb5210d4bb28ed7c1426da855ae
douniabendib <bendib.dounia@gmail.com>
2023-08-29 00:26:06 +0000

    print div

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,12 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (TypeError,ZeroDivisionError):
         result = None
-        print("Error: Division by zero")
-    except Exception as e:
-        result = None
-        print("Error:", e)
     finally:
         print("Inside result: {}".format(result))    
     return result
