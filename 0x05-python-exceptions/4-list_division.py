b99ad7f9cec193574f6f48313ba3fafd34622257
douniabendib <bendib.dounia@gmail.com>
2023-08-29 01:28:08 +0000

    task div

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-result_list = []
+
+new_list = []
     for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
@@ -14,5 +15,5 @@ result_list = []
             print("out of range")
             div = 0
         finally:
-            result_list.append(div)
-    return (result_list)
+            new_list.append(div)
+    return (new_list)
