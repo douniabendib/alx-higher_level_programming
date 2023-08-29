c8c6834e8d49faa7cc710d48cfb60159d03c792e
douniabendib <bendib.dounia@gmail.com>
2023-08-29 00:24:36 +0000

    print div

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,9 +9,5 @@ def safe_print_division(a, b):
         result = None
         print("Error:", e)
     finally:
-        if result is not None:
-            print("Inside result: {}".format(result))
-        else:
-            print("Inside result: None")
-    
+        print("Inside result: {}".format(result))    
     return result
