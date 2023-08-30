ec24bf2778a1b7a22bfe7b014a5f82ad5388e1ae
douniabendib <bendib.dounia@gmail.com>
2023-08-30 23:11:09 +0000

    mqgic task

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
 
-def magic_calculation(a, b):    
-    
+def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
+                raise ValueError("Value of 'i' exceeds 'a'")
             else:
                 result += a ** b / i
-        except:
+        except ValueError as ve:
             result = b + a
             break
-    return (result)
+    return result
