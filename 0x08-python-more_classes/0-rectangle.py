124bdab488452d68b5d062a026b4a01198bcfb24
douniabendib <bendib.dounia@gmail.com>
2023-09-04 21:34:03 +0000

    rectangle class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Define a rectangle class. """
+"""Define a rectangle class."""
 
 
 
 class Rectangle:  
-    """
-    Represent a rectangle
-    """
+    """Represent a rectangle."""
     pass
