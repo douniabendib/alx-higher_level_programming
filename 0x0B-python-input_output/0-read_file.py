2eacb5db7e54ae7c68e2137ea0f85e7bc8d94360
douniabendib <bendib.dounia@gmail.com>
2023-09-12 13:24:40 +0000

    read f

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,7 @@
 
 
 def read_file(filename=""):
-    """print a content of file"""
+    """Print a content of file"""
     with open(filename, "r", encoding="utf-8") as f:
-         read_file = f.read()
-         print(read_file, end ="")
+        read_file = f.read()
+        print(read_file, end ="")
