664066bb126de9f29c1d6a32b558fd66b6581a2f
douniabendib <bendib.dounia@gmail.com>
2023-09-12 14:00:34 +0000

    write text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """Write a strint to file"""
     with open(filename, "w", encoding="utf-8") as f:
-        write_file = f.write()
-        print(write_file, end="")
+        write_file = f.write(text)
+        print(write_file)
