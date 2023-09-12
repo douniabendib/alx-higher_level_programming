a07f302d4f241ffcf66c6170e27310207d8df53a
douniabendib <bendib.dounia@gmail.com>
2023-09-12 10:46:49 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-  with open(filename, 'r', encoding="utf-8") as f:
-	read_file = f.read()
-	print(read_file)
+  try:
+  	with open(filename, 'r', encoding="utf-8") as f:
+		read_file = f.read()
+		print(read_file)
+  except Exception as e:
+	print(f"An error {str(e)}")
