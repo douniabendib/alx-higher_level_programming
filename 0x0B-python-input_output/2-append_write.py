e440c8e5b46eb683649c2632183f82a9e0c4e5fd
douniabendib <bendib.dounia@gmail.com>
2023-09-12 14:59:45 +0000

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-   """how to append a filename""""
+   """how to append a text at end of file""""
    with open(filename, "a", encoding="utf-8") as f:
        return f.write(text)
