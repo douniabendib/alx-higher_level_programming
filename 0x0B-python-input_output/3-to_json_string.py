1efe9ca3aa34d3e72f80067df10305d2d305fb3b
douniabendib <bendib.dounia@gmail.com>
2023-09-12 16:31:19 +0000

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""Define Json sring"""
+
+
+def to_json_string(my_obj):
+    """function to represent json.
+
+    Args:
+        my_obj:the object
+    Returns:
+        json representation
+    """
+    return my_obj
