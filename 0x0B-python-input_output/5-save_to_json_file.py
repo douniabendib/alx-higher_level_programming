5125d20e247dfb6c22e6509922e922eb8dc418e7
douniabendib <bendib.dounia@gmail.com>
2023-09-13 02:24:09 +0000

    save

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define function that writes an Object to a text file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write object file using json representation"""
+    with open(filename, "w") as f:
+	return json.dumps(filename)
