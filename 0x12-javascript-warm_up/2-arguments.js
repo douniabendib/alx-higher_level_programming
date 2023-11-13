d3042c1cdb97ac2e0eb4596e8b11552aaba3084b
douniabendib <bendib.dounia@gmail.com>
2023-11-13 13:32:03 -0800

    argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const command = process.argv.length;
-if (command === 2){
-	console.log('No argument');
+if (command === 2) {
+  console.log('No argument');
+} else if (command === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (command === 3){
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');}
