8f8992163680a08cb5588d6551b7271d94e891e5
douniabendib <bendib.dounia@gmail.com>
2023-11-13 15:05:10 -0800

    multi c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myCommand = process.argv[2];
+const num = Number(myCommand);
+if (myCommand === undefined || isNaN(myCommand)) {
+	console.log('Missing number of occurrences');
+} else {
+	for (let i = 0; i < num; i++)
+	{
+		console.log('C is fun');
+	}}
