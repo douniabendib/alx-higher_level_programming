f8670c18c0647aacd5c7e68f0af7066a0471aa34
douniabendib <bendib.dounia@gmail.com>
2023-11-13 16:06:22 -0800

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 const command = process.argv[2];
 const numOcc = Number(command);
+
 if (command === undefined || isNaN(command)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < numOcc; i++) {
-	  console.log('X'.repeat(numOcc));
-	  }
+    console.log('X'.repeat(numOcc));
+  }
 }
