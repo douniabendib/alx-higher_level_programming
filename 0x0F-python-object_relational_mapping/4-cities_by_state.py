82f99a92e41079484e068c3e7664628e3d42899d
douniabendib <bendib.dounia@gmail.com>
2023-11-19 12:39:59 -0800

    cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQL.connect(
+    db = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=sys.argv[1],
