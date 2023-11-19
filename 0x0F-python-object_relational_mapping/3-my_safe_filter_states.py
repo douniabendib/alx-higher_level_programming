d79fe16786a01766e9d9a243b7001700581688ed
douniabendib <bendib.dounia@gmail.com>
2023-11-19 11:09:02 -0800

    save my filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that takes in arguments and displays all values"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cursor = db.cursor()
+    s_name = sys.argv[4]
+    cursor.execute("""SELECT * FROM WHERE name LIKE
+    %s""", (s_name, ))
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+    cursor.close()
+    db.close()
+
