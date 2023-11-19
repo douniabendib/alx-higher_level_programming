48140b8c5431b3d5e4278552fc5fae5134cf5e97
douniabendib <bendib.dounia@gmail.com>
2023-11-19 12:59:44 -0800

    filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists all cities of that state,using the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQL.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cursor = db.cursor()
+    s_name = sys.argv[4]
+    cursor.execute("""SELECT cities.name FROM cities INNER JOIN
+    states ON states.id=cities.state_id WHERE states=%s""", (s_name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
