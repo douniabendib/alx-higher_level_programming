ef6e715c23a0e6e47428016e30b6259b563b7055
douniabendib <bendib.dounia@gmail.com>
2023-11-19 08:33:03 -0800

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
             )
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE name LIKE 
-            BINARY 'N%' ORDER BY states.id""")
+                    BINARY 'N%' ORDER BY states.id""")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
