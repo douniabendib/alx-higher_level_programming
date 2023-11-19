def566055e9a00090a4007eecd9af40fbc8f8ff2
douniabendib <bendib.dounia@gmail.com>
2023-11-19 08:16:26 -0800

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa"""
+""" script that lists all states with a name starting with N """
 import MySQLdb
 import sys
 
@@ -13,8 +12,8 @@ if __name__ =="__main__":
             db=sys.argv[3]
             )
     cursor = db.cursor()
-    q = """SELECT *FROM states WHERE name LIKE 'N%'
-    ORDER BY states.id"""
+    q = """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY
+    states.id"""
     cursor.execute(q)
     rows = cursor.fetchall()
     for row in rows:
