639e9dcc420c91cda9b170c8dfc4eb645f814cb0
bendibdounia <bendib.dounia@gmail.com>
2023-11-30 17:50:59 -0800

    header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request
-curl "$1" -sX GET -H "X-HolbertonSchool-User-Id:98"
+curl "$1" -sX GET -H "X-School-User-Id:98"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  script that takes in a URL, sends a POST request
-curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
