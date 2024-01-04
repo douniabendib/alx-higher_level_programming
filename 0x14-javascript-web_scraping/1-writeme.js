525d0cbdd23877bacf38348feab4c140a50a1b50
bendibdounia <bendib.dounia@gmail.com>
2024-01-04 14:27:46 -0800

    done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -10,8 +10,8 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
-	if (err) {
-		console.log(err);
-		return (-1);
-	}
+  if (err) {
+    console.log(err);
+    return (-1);
+  }
 });
